f3696dc982d3e763ea3f6c47cd06bdc165040470
Marni <suchermarni@gmail.com>
2020-09-22 17:09:58 -0400

    add questions and choices

--- js/scripts.js
+++ js/scripts.js
@@ -13,5 +13,8 @@ $document.ready(function() {
     const answer9 = $("input:radio[name=question9]:checked").val();
     const answer10 =$("input:radio[name=question10]:checked").val();
 
+    if (answer1 === "1" && (answer2 === "1" || answer3 === "1" || answer4 === "1" ||
+         answer5 === "1" || answer6 === "1" || answer7 === "1" || answer8 === "1" ||
+         answer9 === "1"
   });
 });
