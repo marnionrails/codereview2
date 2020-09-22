e98500e067e65deaa86485963ab2f5936861c25c
Marni <suchermarni@gmail.com>
2020-09-22 15:01:19 -0400

    create scripts.js file

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+$document.ready(function() {
+  $("form#form-group").submit(function(event) {
+    event.preventDefault();
+
+    const answer1 = $("input:radio[name=question1]:checked").val();
+    const answer2 = $("input:radio[name=question2]:checked").val();
+    const answer3 = $("input:radio[name=question3]:checked").val();
+    const answer4 = $("input:radio[name=question4]:checked").val();
+    const answer5 = $("input:radio[name=question5]:checked").val();
+    const answer6 = $("input:radio[name=question6]:checked").val();
+    const answer7 = $("input:radio[name=question7]:checked").val();
+    const answer8 = $("input:radio[name=question8]:checked").val();
+    const answer9 = $("input:radio[name=question9]:checked").val();
+    const answer10 =$("input:radio[name=question10]:checked").val();
+
+  });
+});
