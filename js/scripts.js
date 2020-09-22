7feddb9ef32397464d7f1ee6b734e44ebc1de370
Marni <suchermarni@gmail.com>
2020-09-22 18:15:56 -0400

    add () around document in jquery function

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,17 @@
-$document.ready(function() {
+$(document).ready(function() {
   $("form#form-group").submit(function(event) {
     event.preventDefault();
 
-    let answer1 = $("input:radio[name=question1]:checked").val();
-    let answer2 = $("input:radio[name=question2]:checked").val();
-    let answer3 = $("input:radio[name=question3]:checked").val();
-    let answer4 = $("input:radio[name=question4]:checked").val();
-    let answer5 = $("input:radio[name=question5]:checked").val();
-    let answer6 = $("input:radio[name=question6]:checked").val();
-    let answer7 = $("input:radio[name=question7]:checked").val();
-    let answer8 = $("input:radio[name=question8]:checked").val();
-    let answer9 = $("input:radio[name=question9]:checked").val();
-    let answer10 =$("input:radio[name=question10]:checked").val();
+    const answer1 = $("input:radio[name=question1]:checked").val();
+    const answer2 = $("input:radio[name=question2]:checked").val();
+    const answer3 = $("input:radio[name=question3]:checked").val();
+    const answer4 = $("input:radio[name=question4]:checked").val();
+    const answer5 = $("input:radio[name=question5]:checked").val();
+    const answer6 = $("input:radio[name=question6]:checked").val();
+    const answer7 = $("input:radio[name=question7]:checked").val();
+    const answer8 = $("input:radio[name=question8]:checked").val();
+    const answer9 = $("input:radio[name=question9]:checked").val();
+    const answer10 =$("input:radio[name=question10]:checked").val();
 
     let total = parseInt(answer1) + parseInt(answer2) + parseInt(answer3) + parseInt(answer4) + parseInt(answer5) + parseInt(answer6) + parseInt(answer7) + parseInt(answer8) + parseInt(answer9) + parseInt(answer10);
 
